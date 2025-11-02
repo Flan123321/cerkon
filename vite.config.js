9ac463fa1efef41225c0b5d0d5f5410bd172978a
Flavio C. Figueroa <130038309+Flan123321@users.noreply.github.com>
2025-11-02 13:27:14 -0300

    Configuración final para gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
+// Archivo: vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/cerkon/', // <-- ¡Esta línea es crucial!
   plugins: [
