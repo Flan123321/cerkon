0b2757eb66e270e0e2a2d66a6e5fbc379dffb2ed
Flavio C. Figueroa <130038309+Flan123321@users.noreply.github.com>
2025-11-02 13:31:26 -0300

    Eliminar 'base' path para Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/cerkon/', // <-- ¡Esta línea es crucial!
+  
   plugins: [
     react()
   ],
